02a91fb087b6e26645b23bce151c5e1fd6723e79
Jonathan (JB) Belcher <belcherj@gmail.com>
2019-11-04 14:33:50 -0500

    Build: webapp-staging [ci skip]

--- vip/webapp/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const { server } = require('@automattic/vip-go');
-
-const port = process.env.PORT || 4000;
-
-app.use(express.static('dist'));
-
-server(app, { PORT: port }).listen();
